0951fd71d1b793592f7eed754a338849e1a5df0e
Shivang Madhwal <shivang2790@gmail.com>
2022-08-30 11:02:12 +0530

    empty input done

--- index.js
+++ index.js
@@ -19,10 +19,23 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/:date", function (req, res) {
-	res.json({
-		unix: new Date(req.params.date).getTime(),
-		utc: new Date(req.params.date).toUTCString(),
-	});
+	let passedDate = req.params.date;
+	let date = new Date(passedDate);
+	if (date == "Invalid Date") {
+		res.json({ error: "Invalid Date" });
+	} else {
+		res.json({
+			unix: date.getTime(),
+			utc: date.toUTCString(),
+		});
+	}
+});
+
+app.get("/api", function (req, res) {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  })
 });
 
 // your first API endpoint...
@@ -31,6 +44,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
 	console.log("Your app is listening on port " + listener.address().port);
 });
